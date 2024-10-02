5320aaf8036ce7f532709c1f605261e3b44c344a
fixedNick <kkos44911@gmail.com>
2024-10-02 17:47:50 +0300

    sh upd

--- docker/mysql/init.sh
+++ docker/mysql/init.sh
@@ -10,3 +10,17 @@ else
   mysql -u root -p"$MYSQL_ROOT_PASSWORD" $MYSQL_DATABASE < /docker-entrypoint-initdb.d/taro.sql
   echo "База данных $MYSQL_DATABASE создана и данные импортированы."
 fi
+
+# Проверка, пуста ли таблица horoscope
+if mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "SELECT 1 FROM $MYSQL_DATABASE.horoscope LIMIT 1" 2>/dev/null; then
+  echo "Таблица horoscope не пуста. Данные не будут перенесены."
+else
+  echo "Таблица horoscope пуста. Перенос данных из taro.sql..."
+   # Экспорт данных из таблицы taro.horoscope в файл SQL
+  mysqldump -u root -p"$MYSQL_ROOT_PASSWORD" taro horoscope > /tmp/horoscope_data.sql
+
+  # Импорт данных в таблицу horoscope в базе данных $MYSQL_DATABASE
+  mysql -u root -p"$MYSQL_ROOT_PASSWORD" $MYSQL_DATABASE < /tmp/horoscope_data.sql
+
+  echo "Данные успешно перенесены."
+fi
